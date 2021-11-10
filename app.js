e3ca7b1ed2de3173147fb09b1cb617d3919a2feb
sharan-54321 <72913650+sharan-54321@users.noreply.github.com>
2021-11-10 08:26:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -78,15 +78,13 @@ document.addEventListener("DOMContentLoaded", function () {
         // console.log(sessions.length);
         let nearby_loc = document.createElement("div");
 
-        var loc = `
-                       <h2 class="heading-4">
-                          Vaccination centers near you - <span class="v-centers">${sessions.length}</span>
-                      </h2>
-                    `;
+        //no. of locations
+        var loc = `<h2 class="heading-4"> Vaccination centers near you - <span class="v-centers">${sessions.length}</span> </h2>`;
         nearby_loc.innerHTML = loc;
 
         document.getElementById("cards-parking").appendChild(nearby_loc);
 
+        //vax cards
         for (var i = 0; i < sessions.length; i++) {
           let newCard = document.createElement("div");
           var row = `<div class="vaccine-card" data-aos="slide-up" data-aos-offset="5" data-aos-delay="15" data-aos-duration="1000" data-aos-once="true" data-aos-easing="ease-in-out"> <div class="hospital-details"> <h6 class="heading-3">Hospital/ center Name</h6> <div class="hospital-address"> <span class="hospital-name" ><strong class="bold-text-2">${sessions[i].name}</strong></span >${sessions[i].address}<br/> </div><div class="district-pincode">${sessions[i].district_name}, ${sessions[i].pincode}</div></div><div class="vaccine-details flexbox-first"> <h3 class="heading-2">VACCINE AVAILABLE</h3> <div class="vaccine-name">${sessions[i].vaccine}</div><div class="dose-available"> <div class="dose-details"> <div class="dose-1">available dose 1</div><div class="image"> <img src="https://uploads-ssl.webflow.com/6181638c04f8b017504fb98f/61822c3690bdeb5637e09b3b_arrow_downward_24px.svg" loading="lazy" alt="" class="image-5"/> </div><div class="dose-number">${sessions[i].available_capacity_dose1}</div></div><div class="dose-details"> <div class="dose-1">available dose 2</div><div class="image"> <img src="https://uploads-ssl.webflow.com/6181638c04f8b017504fb98f/61822c3690bdeb5637e09b3b_arrow_downward_24px.svg" loading="lazy" alt="" class="image-5"/> </div><div class="dose-number">${sessions[i].available_capacity_dose2}</div></div></div></div><div class="fee-details"> <div class="fee-typeface">Fee Type</div><div class="free-paid">${sessions[i].fee_type}</div><div class="price-details"> <div class="fee-typeface">Fee</div><div class="rupee"> <img src="https://uploads-ssl.webflow.com/6181638c04f8b017504fb98f/61823131d433804ae579f29e_rupee%202.svg" loading="lazy" alt="" class="image-6"/> </div><div class="price">${sessions[i].fee}</div></div></div></div>`;
